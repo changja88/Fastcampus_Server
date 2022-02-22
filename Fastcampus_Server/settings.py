d2995107343a9dcb165ade5a74565de108913808
HYUN <hyun@HYUNui-MacPro.local>
2022-02-22 22:33:13 +0900

    save

--- Fastcampus_Server/settings.py
+++ Fastcampus_Server/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Fastcampus_Server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'mellowdb.ctvkn9fczt3d.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+        'NAME': 'mellowdb',
+        'USER': 'changja00',
+        'PASSWORD': 'djdnls88',
     }
 }
 
